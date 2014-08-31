4d14da90f178427c2a80b4a5a1b102bc2dcd5298
wrmsr <you@exaple.com>
2014-08-31 12:39:25 -0700

    --

--- src/lai/core.clj
+++ src/lai/core.clj
@@ -1,6 +1,9 @@
 (ns lai.core
   (:import
 
+   com.googlecode.lanterna.TerminalSize
+   com.googlecode.lanterna.TextColor
+   com.googlecode.lanterna.TextColor$RGB
    com.googlecode.lanterna.gui2.DefaultWindow
    com.googlecode.lanterna.gui2.DefaultWindowTextGUI
    com.googlecode.lanterna.gui2.Window
@@ -11,14 +14,18 @@
    com.googlecode.lanterna.screen.Screen
    com.googlecode.lanterna.terminal.DefaultTerminalFactory
    com.googlecode.lanterna.terminal.Terminal
+   com.googlecode.lanterna.terminal.TerminalFactory
    java.nio.charset.Charset
 
    )
   (:gen-class))
 
-(defn -main
-  [& args]
-  (let [terminal-factory (new DefaultTerminalFactory)
+(defn- print-terminal-string [^Terminal terminal ^String string]
+  (doseq [i (range (.length string))]
+    (.putCharacter terminal (.charAt string i))))
+
+(defn -main1 [& args]
+  (let [^TerminalFactory terminal-factory (new DefaultTerminalFactory)
         ^Terminal terminal (.createTerminal terminal-factory)
         ^Screen screen (new DefaultScreen terminal)
         ^WindowBasedTextGUI text-gui (new DefaultWindowTextGUI screen)
@@ -37,3 +44,20 @@
           (recur)))
       (finally
        (.stopScreen screen)))))
+
+(defn -main [& args]
+  (let [^TerminalFactory terminal-factory (new DefaultTerminalFactory)
+        ^Terminal terminal (.createTerminal terminal-factory)
+        ^TerminalSize terminal-size (.getTerminalSize terminal)]
+    (.enterPrivateMode terminal)
+    (.clearScreen terminal)
+    (doseq [i (range 100)]
+      (.setForegroundColor terminal (new TextColor$RGB i i i))
+      (.setBackgroundColor terminal (new TextColor$RGB 0 0 0))
+      (.setCursorPosition terminal 1 i)
+      (print-terminal-string terminal "hi there"))
+    (.flush terminal)
+    (Thread/sleep 200)))
+
+
+
