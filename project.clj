78e4ea9e2ec260e79993f31c76993b463414381e
FUCKYOUGIT <FUCKYOUGIT@FUCKYOU.FUCK>
2014-09-07 15:23:55 -0700

    --

--- project.clj
+++ project.clj
@@ -5,8 +5,11 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [com.taoensso/timbre "3.3.0" :exclusions [org.clojure/clojure]]
-                 [com.googlecode.lanterna/lanterna "3.0.0-alpha3"]]
+                 [com.googlecode.lanterna/lanterna "3.0.0-alpha3"]
+                 [org.clojure/core.typed "0.2.68"]
+                 [org.clojure/data.finger-tree "0.0.2"]]
+  :plugins [[lein-typed "0.3.5"]]
   :main ^:skip-aot lai.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}}
-  :jvm-opts ["-Xmx4g" "-Djava.awt.headless=true"] )
+  :jvm-opts ["-Xmx4g" "-Djava.awt.headless=true"])
--- src/lai/core.clj
+++ src/lai/core.clj
@@ -2,17 +2,14 @@
   (:require [taoensso.timbre :as timbre])
   (:import
 
+   com.googlecode.lanterna.TerminalPosition
    com.googlecode.lanterna.TerminalSize
    com.googlecode.lanterna.TextColor
    com.googlecode.lanterna.TextColor$RGB
-   com.googlecode.lanterna.gui2.DefaultWindow
-   com.googlecode.lanterna.gui2.DefaultWindowTextGUI
-   com.googlecode.lanterna.gui2.Window
-   com.googlecode.lanterna.gui2.WindowBasedTextGUI
-   com.googlecode.lanterna.gui2.WindowManager
-   com.googlecode.lanterna.gui2.WindowManager$Hint
+   com.googlecode.lanterna.graphics.TextGraphics
    com.googlecode.lanterna.screen.DefaultScreen
    com.googlecode.lanterna.screen.Screen
+   com.googlecode.lanterna.screen.Screen$RefreshType
    com.googlecode.lanterna.terminal.DefaultTerminalFactory
    com.googlecode.lanterna.terminal.Terminal
    com.googlecode.lanterna.terminal.TerminalFactory
@@ -27,47 +24,47 @@
   (doseq [i (range (.length string))]
     (.putCharacter terminal (.charAt string i))))
 
-(defn -main [& args]
-  (let [^TerminalFactory terminal-factory (new DefaultTerminalFactory)
-        ^Terminal terminal (.createTerminal terminal-factory)
-        ^Screen screen (new DefaultScreen terminal)
-        ^WindowBasedTextGUI text-gui (new DefaultWindowTextGUI screen)
-        ^Window window (new DefaultWindow "Dialog Test")
-        ^WindowManager window-manager (.getWindowManager text-gui)]
-    (.addWindow window-manager window (into-array WindowManager$Hint nil))
-    (.startScreen screen)
-    (.updateScreen text-gui)
-    (try
-      (loop []
-        (when (not (.isEmpty (.getWindows window-manager)))
-          (.processInput text-gui)
-          (if (.isPendingUpdate text-gui)
-            (.updateScreen text-gui)
-            (Thread/sleep 1))
-          (recur)))
-      (finally
-       (.stopScreen screen)))))
-
-(defn -main1 [& args]
-  (let [^TerminalFactory terminal-factory (new DefaultTerminalFactory)
-        ^Terminal terminal (.createTerminal terminal-factory)
-        ^TerminalSize terminal-size (.getTerminalSize terminal)]
-    (.enterPrivateMode terminal)
-    (.clearScreen terminal)
-    (doseq [^int x (range 255) ^int y (range 100)]
-      (.setForegroundColor terminal (new TextColor$RGB x 0 y))
-      (.setBackgroundColor terminal (new TextColor$RGB x 0 y))
-      (.setCursorPosition terminal x y)
-      (.putCharacter terminal \.))
-    (.flush terminal)
-    (Thread/sleep 200)))
-
 (defn nrepl [port]
   (require 'clojure.tools.nrepl.server)
   (with-out-str
     ((resolve 'clojure.tools.nrepl.server/start-server) :port port))
   (info "nREPL server listening on" port))
 
-(defn -main2 [& args]
+(defrecord Buffer [^String contents])
+(defrecord Window [^int x ^int y ^int width ^int height buffer])
+(defrecord Frame [])
+
+(defn draw-window [^Window window ^TextGraphics text-graphics]
+  (.setForegroundColor text-graphics (new TextColor$RGB 0 0 0))
+  (.setBackgroundColor text-graphics (new TextColor$RGB 0 0 0))
+  (.setPosition text-graphics (new TerminalPosition (inc (:x window)) (inc (:y window))))
+  (.fillRectangle text-graphics (new TerminalSize (- (:width window) 2) (- (:height window) 2)) \space)
+
+  (.setForegroundColor text-graphics (new TextColor$RGB 0 0 0))
+  (.setBackgroundColor text-graphics (new TextColor$RGB 255 255 255))
+  (.setPosition text-graphics (new TerminalPosition (:x window) (:y window)))
+  (.drawRectangle text-graphics (new TerminalSize (:width window) (:height window)) \|)
+
+  (when-let [buffer (:buffer window)]
+    (.setForegroundColor text-graphics (new TextColor$RGB 255 255 255))
+    (.setBackgroundColor text-graphics (new TextColor$RGB 0 0 0))
+    (.setPosition text-graphics (new TerminalPosition (inc (:x window)) (inc (:y window))))
+    (.putString text-graphics (:contents buffer))))
+
+(defn -main [& args]
   (nrepl 7888)
-  (info "Hi there"))
+  (let [^TerminalFactory terminal-factory (new DefaultTerminalFactory)
+        ^Terminal terminal (.createTerminal terminal-factory)
+        ^TerminalSize terminal-size (.getTerminalSize terminal)
+        ^Screen screen (new DefaultScreen terminal)
+        ^TextGraphics text-graphics (.newTextGraphics screen)]
+    (.startScreen screen)
+    (try
+      (do
+        (draw-window (Window. 10 10 100 40 (Buffer. "hi there")) text-graphics)
+        (.refresh screen Screen$RefreshType/DELTA)
+        (Thread/sleep 1000))
+      (finally
+       (.stopScreen screen))))
+  (shutdown-agents)
+  (System/exit 0))
